f2493f45841047f46b4ff3af34a89a7f62bb28e6
Matthieu Degré <matthieu.degre@gmail.com>
2020-02-17 18:30:08 +0100

    before installing devise 2

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
-  validates :start_date, presence: true # +custom validation only > Time.now
-  validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 } # + custom validate number % 5 == 0 et positif
+  validates :start_date, presence: true
+  validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :title, presence: true, length: {minimum: 5, maximum: 140}
   validates :description, presence: true, length: {minimum: 20, maximum: 1000}
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 1000 }
