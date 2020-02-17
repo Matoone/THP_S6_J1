95bb78a37784ec2538dc69ef43aedbdb351a3b43
Matthieu Degré <matthieu.degre@gmail.com>
2020-02-17 17:28:17 +0100

    Added send email at event attendance

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,9 @@
 class Attendance < ApplicationRecord
+  after_create :attendance_send
   belongs_to :attendee, class_name: 'User'
   belongs_to :event
+
+  def attendance_send
+    UserMailer.attendance_email(self.attendee, self.event).deliver_now
+  end
 end
