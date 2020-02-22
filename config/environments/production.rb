60980e3113a18df8d9c89e8f3925f08b63b71cac
Matthieu Degré <matthieu.degre@gmail.com>
2020-02-22 15:56:52 +0100

    changé mailers en https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :mailjet
-  config.action_mailer.default_url_options = { :host => 'eventbritelikebymatoone.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://eventbritelikebymatoone.herokuapp.com' }
 end
